065cd7ba64e10f4a541e4e54824ddecb715466b0
Edwin Guajardo <edwin.guajardo@gmail.com>
2021-05-20 19:16:18 -0500

    Add hardhat environment

---
+++ hardhat.config.ts
@@ -0,0 +1,27 @@
+import { HardhatUserConfig } from "hardhat/types";
+
+import { task } from "hardhat/config";
+import "@nomiclabs/hardhat-waffle";
+
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{ version: "0.8.3", settings: {} }],
+  },
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: 10000
+      },
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/ffb37bda4b4f4641923589a663bdee8f",
+      accounts: [process.env.ROPSTEN_ACCOUNT_1!, process.env.ROPSTEN_ACCOUNT_2!, process.env.ROPSTEN_ACCOUNT_3!, process.env.ROPSTEN_ACCOUNT_4!, process.env.ROPSTEN_ACCOUNT_5!]
+    }
+  }
+};
+export default config;
